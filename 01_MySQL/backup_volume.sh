1098af91b28afa5d4d83eb9705cfc4a7c72018c4
Polarbear0808 <polaris1771@gmail.com>
2021-12-25 10:57:45 +0900

    update: changed backup directory

--- 01_MySQL/backup_volume.sh
+++ 01_MySQL/backup_volume.sh
@@ -5,5 +5,5 @@ BACKUP_TAR_NAME=$1
 docker run \
     --rm \
     --volumes-from mysql1 \
-    --volume $PWD:/backup \
+    --volume $PWD/backup:/backup \
     busybox tar cvf /backup/${BACKUP_TAR_NAME}.tar /var/lib/mysql
