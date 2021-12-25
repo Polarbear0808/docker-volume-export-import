fabb68e54544b766324a5c5bc87a449d9c4c9d2d
Polarbear0808 <polaris1771@gmail.com>
2021-12-25 10:53:01 +0900

    update: made backup tar name variable

--- 01_MySQL/backup_volume.sh
+++ 01_MySQL/backup_volume.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+set -eu
+BACKUP_TAR_NAME=$1
+
 docker run \
     --rm \
     --volumes-from mysql1 \
     --volume $PWD:/backup \
-    busybox tar cvf /backup/backup.tar /var/lib/mysql
+    busybox tar cvf /backup/${BACKUP_TAR_NAME}.tar /var/lib/mysql
